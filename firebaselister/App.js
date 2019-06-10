140a437aac8ad6f78e8dfd511380689cd2a1354f
Abhinav Tiwari <abhi.kay.tiwari@gmail.com>
2019-06-09 20:51:53 -0600

    added toolbar and set up app functionality

--- firebaselister/App.js
+++ firebaselister/App.js
@@ -1,49 +1,52 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Platform, StyleSheet, Text, View, ListView, TouchableHighlight} from 'react-native';
+import Toolbar from './app/components/toolbar/Toolbar';
+const styles = require('./app/style');
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
 
-type Props = {};
-export default class App extends Component<Props> {
+class App extends Component {
+  constructor(){
+  super();
+
+  let ds = new ListView.DataSource({rowHasChanged:(r1, r2) => r1 !== r2})
+  this.state = {
+    itemDataSource: ds
+  }
+}
+  componentWillMount(){
+    this.getItems();
+  }
+
+  componentDidMount(){
+    this.getItems();
+  }
+
+  getItems(){
+    let items = [{title: 'Item One'}, {title: 'Item Two'}];
+    this.setState({
+      itemDataSource: this.state.itemDataSource.cloneWithRows(items)
+    })
+  }
+
+  renderRow = (item) => {
+    return (
+      <TouchableHighlight onPress={() => {this.pressRow(item)}}>
+      <View style={styles.li}>
+      </View>
+      </TouchableHighlight>
+    )
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <Toolbar title={'ItemLister'}/>
+        <ListView dataSource={this.state.itemDataSource} renderRow={this.renderRow} />
+        <Text>'Item Lister'</Text>
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+export default App;
---
+++ firebaselister/app/components/toolbar/Toolbar.js
@@ -0,0 +1,47 @@
+import React, {Component} from 'react';
+import { View, Text, AppRegistry, StatusBar } from 'react-native';
+
+class Toolbar extends Component{
+    state = {
+
+    }
+
+    render(props){
+        return(
+            <View>
+                {/* <StatusBar backgroundColor={'coral'} /> */}
+                <View style={styles.navbar}>
+                <Text style={styles.navbarTitle}>
+                    {this.props.title}
+                </Text>
+                </View>
+            </View>
+        )
+    }
+}
+
+export default Toolbar;
+
+const styles = {
+        navbar: {
+            alignItems: 'center',
+            backgroundColor: '#fff',
+            borderBottomColor: '#eee',
+            borderColor: 'transparent',
+            borderWidth: 1,
+            justifyContent: 'center',
+            alignSelf: 'center',
+            height: 44,
+            flexDirection: 'row'
+        },
+        navbarTitle: {
+            color: '#444',
+            fontSize: 16,
+            fontWeight: '600'
+        },
+        toolbar: {
+            backgroundColor: '#fff',
+            height: 22
+        }
+}
+
